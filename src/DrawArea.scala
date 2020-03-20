64ccfcb335ba1d2db2a899b4f785c42137fb5634
elias <elias@192.168.1.101>
2020-03-20 13:26:05 +0200

    "init"

---
+++ src/DrawArea.scala
@@ -0,0 +1,14 @@
+import scala.swing._
+import java.awt.Color
+import java.awt.{ Point, Rectangle }
+import scala.io.Source
+import javax.imageio.ImageIO
+import java.io.File
+import java.awt.image.BufferedImage
+import scala.swing.event._
+
+
+class DrawArea extends Component{
+  var Image: Image
+  
+}
---
+++ src/Interface.scala
@@ -0,0 +1,159 @@
+import scala.swing._
+import java.awt.Color
+import java.awt.{ Point, Rectangle }
+import scala.io.Source
+import javax.imageio.ImageIO
+import java.io.File
+import java.awt.image.BufferedImage
+import scala.swing.event._
+
+
+object Interface extends SimpleSwingApplication {
+  
+  
+  val newButton   = new Button("New") 
+  val undoButton  = new Button("Undo")
+  val redoButton  = new Button("Redo")
+  val colorButton = new Button("Color")
+  colorButton.background = new Color(255,255,255)
+  val shapeButton = new Button("Shape")
+  
+  colorButton.reactions += {
+    case clickEvent: ButtonClicked => 
+      colorPanel.open()
+  }
+  
+  val topMenu = new MainFrame {
+    title = "Draw"
+    resizable = false
+    
+    val width      = 500
+    val height     = 600
+    val fullHeight = 600
+    
+    minimumSize   = new Dimension(width,fullHeight)
+    preferredSize = new Dimension(width,fullHeight)
+    maximumSize   = new Dimension(width,fullHeight)
+    
+    } 
+  
+    val topBar     = new FlowPanel
+    topBar.contents += newButton
+    topBar.contents += undoButton
+    topBar.contents += redoButton
+    topBar.contents += colorButton
+    topBar.contents += shapeButton
+    
+    top.contents = topBar
+    
+  val colorPanel = new Frame {
+      val width = 250
+      val height = 200
+      preferredSize = new Dimension(width,height)
+      
+      contents = new GridPanel(4,4) {
+        val redButton = new Button("") 
+        redButton.background = new Color(204,0,0)
+        val lRedButton = new Button("")
+        lRedButton.background = new Color(255,0,0)
+        val blueButton = new Button("")
+        blueButton.background = new Color(0,0,204)
+        val lBlueButton = new Button("")
+        lBlueButton.background = new Color(0,0,255)
+        val tealButton = new Button("")
+        tealButton.background = new Color(0,204,204)
+        val lTealButton = new Button("")
+        lTealButton.background = new Color(0,255,255)
+        val greenButton = new Button("")
+        greenButton.background = new Color(102,204,0)
+        val lGreenButton = new Button("")
+        lGreenButton.background = new Color(128,255,0)
+        val yellowButton = new Button("")
+        yellowButton.background = new Color(204,204,0)
+        val lYellowButton = new Button("")
+        lYellowButton.background = new Color(255,255,0)
+        val orangeButton = new Button("")
+        orangeButton.background = new Color(204,102,0)
+        val lOrangeButton = new Button("")
+        lOrangeButton.background = new Color(255,128,0)
+        val purpleButton = new Button("")
+        purpleButton.background = new Color(204,0,204)
+        val lPurpleButton = new Button("")
+        lPurpleButton.background = new Color(255,0,255)
+        val greyButton = new Button("")
+        greyButton.background = new Color(128,128,128)
+        val blackButton = new Button("")
+        blackButton.background = new Color(255,255,255)
+        val whiteButton = new Button("")
+        whiteButton.background = new Color(0,0,0)
+        
+        contents ++= Vector(
+            redButton,
+            lRedButton,
+            orangeButton,
+            lOrangeButton,
+            yellowButton,
+            lYellowButton,
+            greenButton,
+            lGreenButton,
+            tealButton,
+            lTealButton,
+            blueButton, 
+            lBlueButton,
+            lPurpleButton,
+            greyButton,
+            blackButton,
+            whiteButton
+            )
+            
+        listenTo(redButton,
+            lRedButton,
+            orangeButton,
+            lOrangeButton,
+            yellowButton,
+            lYellowButton,
+            greenButton,
+            lGreenButton,
+            tealButton,
+            lTealButton,
+            blueButton, 
+            lBlueButton,
+            lPurpleButton,
+            greyButton,
+            blackButton,
+            whiteButton)
+        reactions += {
+          case ButtonClicked(`redButton`)     => retColor(redButton)
+          case ButtonClicked(`lRedButton`)    => retColor(lRedButton)
+          case ButtonClicked(`orangeButton`)  => retColor(orangeButton)
+          case ButtonClicked(`lOrangeButton`) => retColor(lOrangeButton)
+          case ButtonClicked(`yellowButton`)  => retColor(yellowButton)
+          case ButtonClicked(`lYellowButton`) => retColor(lYellowButton)
+          case ButtonClicked(`greenButton`)   => retColor(greenButton)
+          case ButtonClicked(`lGreenButton`)  => retColor(lGreenButton)
+          case ButtonClicked(`tealButton`)    => retColor(tealButton)
+          case ButtonClicked(`lTealButton`)   => retColor(lTealButton)
+          case ButtonClicked(`blueButton`)    => retColor(blueButton)
+          case ButtonClicked(`lBlueButton`)   => retColor(lBlueButton)
+          case ButtonClicked(`lPurpleButton`) => retColor(lPurpleButton)
+          case ButtonClicked(`greyButton`)    => retColor(greyButton)
+          case ButtonClicked(`blackButton`)   => retColor(blackButton)
+          case ButtonClicked(`whiteButton`)   => retColor(whiteButton)
+        }
+      }
+      
+      
+      def retColor(button: Button) = {
+        Interface.colorButton.background = button.background
+        this.close()
+      }
+      
+  }
+    
+  
+
+    
+  def top = this.topMenu
+    
+   
+}
